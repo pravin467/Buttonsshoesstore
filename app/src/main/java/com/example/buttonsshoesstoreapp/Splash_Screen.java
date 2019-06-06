2a365aa816b9287c7c0d498cebe26e1428ceca27
pravin467 <boharaprabin@gmail.com>
2019-06-07 00:23:15 +0545

    first commit

--- app/src/main/java/com/example/buttonsshoesstoreapp/Splash_Screen.java
+++ app/src/main/java/com/example/buttonsshoesstoreapp/Splash_Screen.java
@@ -1,5 +1,7 @@
 package com.example.buttonsshoesstoreapp;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,16 @@ public class Splash_Screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash__screen);
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(Splash_Screen.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+
+
+            }
+        },5000);
     }
 }
